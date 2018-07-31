92a79656679b43b1585b28e9963f6efe1e42f0f8
matheusmastrangelo <mastrangeloafi@hotmail.com>
2018-07-30 22:55:43 -0300

    commit:   empdetalhe-2.sql 	commit:   home/sobre.php 	commit:   config/connect_base.php 	commit:   config/constants.php

--- home/sobre.php
+++ home/sobre.php
@@ -3,7 +3,11 @@
 include_once('../header.php');
 include_once('topo.php');
 ?>
-
+<?php
+    $query='SELECT * FROM empdetalhe LIMIT 1';
+    $resultado = mysql_query($query);
+    $fetch = mysql_fetch_array($resultado);
+?>
 <section class="colon14">
 
     <div class="singleheader">
@@ -29,15 +33,13 @@ include_once('topo.php');
 
         <div id="content" class="single col-md-12">
             <div class="image-container img-responsive">
-                <img src="../img/quem_somos.jpg" alt="">
+                <img src="<?php echo URL_BASE.'img/'.$fetch['ds_imgtopo'];?>" alt="">
             </div>
             <div class="text-left" >
                 <h2 style="color:#1d4781;"> <strong> Quem somos? </strong> </h2>  
             </div>
             <div class="col-md-12">
-                <p>Os Interact Clubs são grupos de adolescentes de 12 a 18 anos, patrocinados por um Rotary Club, em que o principal objetivo é tentar ajudar a sociedade de uma forma simples, com campanhas, doações, visitas a creches e hospitais, entre outras coisas para melhorar do meio-ambiente à saúde da população carente.</p>
-                <p>A sigla Interact significa International Action, ou Ação Internacional. O primeiro Interact Club foi fundado em 1962, em Melbourne, na Flórida, e rapidamente se espalhou pelo mundo afora. Atualmente, existem Interact Clubs espalhados em 135 países e regiões geográficas, e são 326.186 interactianos distribuídos em 14.182 Interact Clubs. No Brasil, existem 1500 Interact Clubs e 12.400 Interactianos em 38 distritos rotários.</p>
-                <p>Enquanto se empenham nesses objetivos, os jovens estarão fazendo amizades duradouras e, ao mesmo tempo, estarão desenvolvendo o espírito de equipe e liderança, pois todos os Interacts contam com um disciplinado Conselho Diretor, escolhido pelos próprios jovens em votações. E, com isso, todos os interactianos estão sujeitos e serem escolhidos para cargos como: presidente, vice, secretário, tesoureiro, protocolo e avenidas.</p>
+                <?php echo nl2br($fetch['ds_txttopo']);?>
                 <hr>
             </div>
 
@@ -47,7 +49,8 @@ include_once('topo.php');
             </div>
 
             <div class="col-md-6 img-responsive">
-                <img src="../img/quem_somos.jpg" alt="">
+                <img src="<?php echo URL_BASE.'img/'.$fetch['ds_imgfina'];?>" alt="">
+                
             </div>
 
             <div  class="col-md-6 text-left">
@@ -73,13 +76,7 @@ include_once('topo.php');
             <div class="col-md-12">
                 &nbsp;
                 <hr>
-                <p> Além da alegria em ajudar ao próximo, interactianos:</p>
-                <ul>
-                    <li>Fazem palestras em reuniões de clube; acompanham rotarianos para conhecer mais sobre diversas profissões; e comparecem a feiras de colocação no mercado de trabalho organizadas por Rotary Clubs.</li>
-                    <li>Ampliam seus horizontes ao estar em contato com aqueles aos quais prestam assistência idosos, crianças com deficiências, sem-teto e muitas outras pessoas carentes.</li>
-                    <li>Desenvolvem senso de responsabilidade e ética profissional ao organizar campanhas de preservação do meio ambiente, de erradicação da pólio, anti-drogas, e para solução de problemas da comunidade.</li>
-                    <li>Aprendem sobre outras culturas por intermédio do contato e do trabalho com interactianos do exterior.</li>
-                </ul>
+                    <?php echo nl2br($fetch['ds_txtfina']);?>
                 <hr>
             </div>
         </div><!-- end content -->
@@ -92,26 +89,26 @@ include_once('topo.php');
 
             <div class="col-lg-3 col-md-3 col-sm-6" data-effect="slide-bottom">
                 <div class="stat">
-                    <span class="stat-count">10</span>
-                    <p class="stat-detail">Anos InteractLP</p>
+                    <span class="stat-count"><?php echo nl2br($fetch['nr_curios1'])?></span>
+                    <p class="stat-detail">  <?php echo nl2br($fetch['ds_curios1']);?></p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-6" data-effect="slide-bottom">
                 <div class="stat">
-                    <span class="stat-count">100</span>
-                    <p class="stat-detail">Interactianos</p>
+                    <span class="stat-count"><?php echo nl2br($fetch['nr_curios2'])?></span>
+                    <p class="stat-detail"><?php echo nl2br($fetch['ds_curios2']);?></p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-6" data-effect="slide-bottom">
                 <div class="stat">
-                    <span class="stat-count">300</span>
-                    <p class="stat-detail">Instituições cadastradas</p>
+                    <span class="stat-count"><?php echo nl2br($fetch['nr_curios3'])?></span>
+                    <p class="stat-detail"><?php echo nl2br($fetch['ds_curios3']);?></p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-6" data-effect="slide-bottom">
                 <div class="stat">
-                    <span class="stat-count">35</span>
-                    <p class="stat-detail">Patrocinadores</p>
+                    <span class="stat-count"><?php echo nl2br($fetch['nr_curios4'])?></span>
+                    <p class="stat-detail"><?php echo nl2br($fetch['ds_curios4']);?></p>
                 </div>
             </div>
         </div>
